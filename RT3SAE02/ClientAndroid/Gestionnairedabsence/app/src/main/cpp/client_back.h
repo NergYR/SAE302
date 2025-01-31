8c819ceea2a80c8ad0b2cf7b9fa22be43cdbcc27
Théo HUGUET <theo.huguet@india-01.com>
2025-01-31 15:04:50 +0100

    Ajout du code Android Studio

---
+++ RT3SAE02/ClientAndroid/Gestionnairedabsence/app/src/main/cpp/client_back.h
@@ -0,0 +1,2 @@
+void error(const char *msg);
+int connect_to_server(const char *hostname, int portno, int *sockfd);
---
+++ RT3SAE02/ClientAndroid/Gestionnairedabsence/app/src/main/java/com/example/gestionnairedabsence/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.gestionnairedabsence;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Récupération des boutons
+        Button buttonSecure = findViewById(R.id.bouton_connexion_secure);
+        Button buttonNonSecure = findViewById(R.id.bouton_connexion_non_secure);
+
+        // Redirection vers l'authentification sécurisée
+        buttonSecure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AuthentificationSecureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Redirection vers l'authentification non sécurisée
+        buttonNonSecure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AuthentificationNonSecureActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
