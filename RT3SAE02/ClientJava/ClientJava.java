7bc90a485fa9f3e25670f592ba7e093df029588a
CookiePowa <clement.bulte@gmail.com>
2025-02-01 16:59:42 +0100

    Le client java demandé est terminé

--- RT3SAE02/ClientJava/ClientJava.java
+++ RT3SAE02/ClientJava/ClientJava.java
@@ -27,12 +27,16 @@ public class ClientJava {
     public void Run(){
         CommServ cs = new CommServ(this.addr);
         List<String> name = cs.GetInfo();
-        System.out.println(name);
-        cs.SendInfo("RT1FA.csv\n");
+        for(int i=0;i<name.size();i++){
+            System.out.println(name.get(i));
+        }
+        Scanner s = new Scanner(System.in);
+        System.out.println("Entrez le nom d'un fichier (avec son extention)");
+        String file_name = s.nextLine();
+        cs.SendInfo(file_name+"\n");
         List<List<String>> file = cs.GetFile();
         Presence p = new Presence(file);
         p.Console_MarkPresenceStudent(p.Appel());
-        System.out.println(" 6");
         cs.SendFile(p.getList());
         cs.Disconnect();
     }
